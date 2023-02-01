47956673d8e59263be87eec3e64d548a3afd221c
Shady Nawara <shady.nawara@outlook.com>
2023-01-31 19:47:44 -0600

    add plus+enter combo to toggle num lock

--- keyboards/pimoroni/keybow2040/config.h
+++ keyboards/pimoroni/keybow2040/config.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#define COMBO_COUNT 1
+
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP4
 #define I2C1_SCL_PIN GP5
--- keyboards/pimoroni/keybow2040/keymaps/default/keymap.c
+++ keyboards/pimoroni/keybow2040/keymaps/default/keymap.c
@@ -11,6 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───|───┼───┤---│
      * │ 0 | . │ + │ENT│
      * └───┴───┴───┴───┘
+     * Plus and Enter to toggle num lock
      */
     [0] = LAYOUT_numpad_4x4(
         KC_P0,     KC_P1,   KC_P4,   KC_P7,
@@ -18,4 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PPLS,   KC_P3,   KC_P6,   KC_P9,
         KC_PENT,   KC_PMNS, KC_PAST, KC_PSLS
     )
+};
+
+const uint16_t PROGMEM numlock_combo[] = {KC_PPLS, KC_PENT, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(numlock_combo, KC_NUM_LOCK)
 };
